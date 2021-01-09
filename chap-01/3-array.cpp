9f84628c12087c6dda3111a98f6245d6bf628cf5
LyKell <kev_dev@hotmail.com>
2021-01-10 00:23:03 +0100

    Complete Chapter 1

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <vector>
 
-int main()
+int main(int argc, char** argv)
 {
-    int array[] = { 0, 1, 2, 3 };
-
-    for (int i = 0; i < 4; ++i)
-    {
-        std::cout << array[i] << std::endl;
+    if (argc < 2) {
+        std::cerr << "Not enough arguments" << std::endl;
+        return -1;
     }
+
+    int length = std::stoi(argv[1]);
+    std::cout << "Length: " << length << std::endl;
+
+    // int* array = new int[length] {};
+
+    std::vector<int> array;
+
+    for (int i = 0; i < length; i++) {
+        // array[i] = i + 1;
+        array.emplace_back(i + 1);
+    }
+
+    // for (int i = 0; i < 4; ++i)
+    // for (int i = 0; i < length; i++)
+    for (int value : array)
+    {
+        std::cout << value << std::endl;
+    }
+
+    // delete[] array;
 
     return 0;
 }
