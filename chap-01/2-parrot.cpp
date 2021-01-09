275b0d52dd9b3af9aac0b4345f833e58939a879e
LyKell <kev_dev@hotmail.com>
2021-01-10 00:23:03 +0100

    Complete Chapter 1

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,31 @@
 // Implement the program here.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    std::string word;
+
+    // while (true) {
+    //     std::cin >> number;
+    //     std::cout << "Craow " << number << "!" << std::endl;
+
+    //     if (number == -1) {
+    //         break;
+    //     }
+    // }
+
+    // do {
+    //     std::cin >> number;
+    //     std::cout << "Craow " << number << "!" << std::endl;
+
+    // } while (number != -1);
+
+    for (int i = 0; i < 10; i++) {
+        cin >> word;
+        cout << "Craow " << word << "!" << endl;
+    }
+
+    return 0;
+}
